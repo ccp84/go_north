afe7881a3544d2b9d602531547c1cc4940160fbe
ccp84 <ccp84@icloud.com>
2022-10-28 18:49:24 +0000

    Py>Add verify username function

--- run.py
+++ run.py
@@ -3,3 +3,10 @@ def display_title_screen():
     Prints out the title screen of the game
     """
     print("Title screen function running")
+
+
+def verify_username():
+    """
+    Take in username and check contents
+    """
+    print("Username verification running")
