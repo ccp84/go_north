b897927c52cd568c74a2c320d2d6d48c6b9aaa12
ccp84 <ccp84@icloud.com>
2022-10-28 18:48:14 +0000

    Py>Add title screen function

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def display_title_screen():
+    """
+    Prints out the title screen of the game
+    """
+    print("Title screen function running")
