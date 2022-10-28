88578f3cfcc36df036bef511447d40f397e75f79
ccp84 <ccp84@icloud.com>
2022-10-28 19:41:24 +0000

    Bug fix>Added int to front of choice variable

--- run.py
+++ run.py
@@ -17,9 +17,9 @@ def verify_first_choice():
     Check user selects 1 or 2 and then respond with action
     """
     choice = input("Start Game - 1   How to play - 2\n ")
-    if choice == 1:
+    if int(choice) == 1:
         start_game()
-    elif choice == 2:
+    elif int(choice) == 2:
         display_instructions()
     else:
         print("ERROR please enter 1 or 2")
