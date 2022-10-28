901c1b7038bb078af9e19cb2a8f753d539d302b1
ccp84 <ccp84@icloud.com>
2022-10-28 18:50:59 +0000

    Py>Add verify first choice function

--- run.py
+++ run.py
@@ -10,3 +10,17 @@ def verify_username():
     Take in username and check contents
     """
     print("Username verification running")
+
+
+def verify_first_choice():
+    """
+    Check user selects 1 or 2 and then respond with action
+    """
+    choice = input("Start Game - 1   How to play - 2\n")
+    if choice == 1:
+        start_game()
+    elif choice == 2:
+        display_instructions()
+    else:
+        print("ERROR please enter 1 or 2")
+        verify_first_choice()
