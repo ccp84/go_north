2df118e6d3bf7548eda94f48c5308f7c38c28984
ccp84 <ccp84@icloud.com>
2022-11-01 20:56:03 +0000

    Py>Add shuffle to story builder

--- story.py
+++ story.py
@@ -1,10 +1,15 @@
 import random
 
 def build_story():
+    storyline = []
     with open("positive.txt") as positive:
         for line in positive:
             new_line = line.strip('\n')
-            print(new_line.split("*"))
+            storyline.append(new_line)
+    print(storyline)
+    random.shuffle(storyline)
+    print(storyline)
+
 
 
 build_story()
