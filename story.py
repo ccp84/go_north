d381c4d8ccbecf6fea3ee4e80e8eecdf7b96dbfb
ccp84 <ccp84@icloud.com>
2022-11-03 11:38:37 +0000

    Txt>Update negative txt file to new list format

--- story.py
+++ story.py
@@ -2,12 +2,20 @@ import random
 
 def build_story():
     storyline = []
+    positives = []
+    negatives = []
     with open("positive.txt") as positive:
         for line in positive:
             new_line = line.strip('\n')
-            storyline.append(new_line)
-    random.shuffle(storyline)
-    print(storyline)
+            positives.append(new_line)
+    with open("negative.txt") as negative:
+        for line in negative:
+            new_line = line.strip('\n')
+            negatives.append(new_line)
+    random.shuffle(positives)
+    random.shuffle(negatives)
+    print(positives[0])
+    print(negatives)
 
 
 
