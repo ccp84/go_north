47d2d95a4fd408f1e711e03935f8753e2d43ea58
ccp84 <ccp84@icloud.com>
2022-11-03 15:09:14 +0000

    Py>Fix maths of positive options while loop

--- story.py
+++ story.py
@@ -18,9 +18,9 @@ def build_story():
     storyline = {}
     while current_level < 4:
         level_options = []
-        i = 0
-        while i < 3:
-            level_options.append(positives[i])
+        i = 1
+        while i <= 3:
+            level_options.append(positives[i + (i * current_level)])
             i += 1
         level_options.append(negatives[current_level])
         random.shuffle(level_options)
@@ -28,6 +28,15 @@ def build_story():
             "a": level_options[0], "b": level_options[1], "c": level_options[2], "d": level_options[3]
             }
         current_level += 1
+    #ending = []
+    # with open("winners.txt") as winners:
+    #     for line in winners:
+    #         new_line = line.strip('\n')
+    #         ending.append(new_line)
+    # random.shuffle(ending)
+    # storyline[current_level] = {
+
+    # }
     print(storyline)
 
 
